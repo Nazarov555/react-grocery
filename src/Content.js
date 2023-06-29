f729f707d4858693760e3fea75992aaacade3469
Nazarov555 <nzrvkeskil@mail.ru>
2023-06-29 14:15:47 +0300

    first commit

---
+++ src/Content.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FaTrashAlt } from 'react-icons/fa';
+
+const Content = () => {
+  return (
+    <main>
+      <FaTrashAlt/>
+    </main>
+  )
+}
+
+export default Content
